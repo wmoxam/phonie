b3ffd7780672597d6967f6afd2448b525f702f19
Lance Ivy <lance@cainlevy.net>
2013-04-28 22:32:18 -0400

    Country#parse should try for the most complete matches first

--- lib/phonie/country.rb
+++ lib/phonie/country.rb
@@ -66,12 +66,12 @@ module Phonie
     end
 
     def parse(number, default_area_code)
-      parts = if md = number.match(number_regex)
-        {:area_code => default_area_code, :number => md[1]}
+      parts = if md = number.match(full_number_regex)
+        {:area_code => md[2], :number => md[-1]}
       elsif md = number.match(area_code_number_regex)
         {:area_code => md[1], :number => md[-1]}
-      elsif md = number.match(full_number_regex)
-        {:area_code => md[2], :number => md[-1]}
+      elsif md = number.match(number_regex)
+        {:area_code => default_area_code, :number => md[1]}
       else
         {}
       end
