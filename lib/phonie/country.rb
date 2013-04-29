d45a3115ff35daa17e8fcfb72290dc5bb02d9cbe
Lance Ivy <lance@cainlevy.net>
2013-04-28 22:45:07 -0400

    the char_3_code is actually the iso 3166-1 alpha-2 code

--- lib/phonie/country.rb
+++ lib/phonie/country.rb
@@ -1,19 +1,19 @@
 module Phonie
-  class Country < Struct.new(:name, :country_code, :char_2_code, :char_3_code, :area_code, :local_number_format, :mobile_format, :full_number_length, :number_format)
+  class Country < Struct.new(:name, :country_code, :char_2_code, :iso_3166_code, :area_code, :local_number_format, :mobile_format, :full_number_length, :number_format)
     def self.load
       data_file = File.join(File.dirname(__FILE__), 'data', 'phone_countries.yml')
 
       all = []
-      YAML.load(File.read(data_file)).each_pair do |key, c|
+      YAML.load(File.read(data_file)).values.each do |c|
         next unless c[:area_code] && c[:local_number_format]
-        all << Country.new(c[:name], c[:country_code], c[:char_2_code], c[:char_3_code], c[:area_code], c[:local_number_format], c[:mobile_format], c[:full_number_length], c[:number_format])
+        all << Country.new(c[:name], c[:country_code], c[:char_2_code], c[:iso_3166_code], c[:area_code], c[:local_number_format], c[:mobile_format], c[:full_number_length], c[:number_format])
       end
       all
     end
 
     COUNTRIES = self.load
     COUNTRIES_BY_PHONE_CODE   = COUNTRIES.inject(Hash.new){|h, c| (h[c.country_code] ||= []) << c; h }
-    COUNTRIES_BY_COUNTRY_CODE = Hash[*COUNTRIES.map{|c| [c.char_3_code.downcase, c] }.flatten]
+    COUNTRIES_BY_COUNTRY_CODE = Hash[*COUNTRIES.map{|c| [c.iso_3166_code.downcase, c] }.flatten]
     COUNTRIES_BY_NAME         = Hash[*COUNTRIES.map{|c| [c.name.downcase, c] }.flatten]
 
     def self.find_all_by_phone_code(code)
