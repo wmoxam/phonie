9194e756aa838654d392984557ce4f1e40ed7eed
Piotr Usewicz <piotr@layer22.com>
2013-07-25 16:07:15 +0100

    Support Mexico

---
+++ test/countries/mx_test.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+## Mexico
+class MXTest < Phonie::TestCase
+  def test_local
+    parse_test('+52 33 36159895',  '52', '33',  '36159895', 'Mexico', false)
+    parse_test('+52 686 3615989',  '52', '686',  '3615989', 'Mexico', false)
+  end
+
+  def test_mobile
+    # Mobile phones have extra 1 in front
+    parse_test('+52 1 55 12345678', '52', '155', '12345678', 'Mexico', true)
+    parse_test('+52 1 444 1234567', '52', '1444', '1234567', 'Mexico', true)
+  end
+end
