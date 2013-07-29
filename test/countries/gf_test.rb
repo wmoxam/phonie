2a9c87b223a1cd6771a9f2e911cfd0c7ea50cfe0
Piotr Usewicz <piotr@layer22.com>
2013-07-29 15:28:42 +0100

    Fix French Guiana

--- test/countries/gf_test.rb
+++ test/countries/gf_test.rb
@@ -3,11 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 ## French Guiana
 class GFTest < Phonie::TestCase
   def test_local
-    parse_test('+5945941234', '594', '594', '1234', 'French Guiana', false)
+    parse_test('+594594123456', '594', '594', '123456', 'French Guiana', false)
   end
 
   def test_mobile
-    parse_test('+5946941234', '594', '694', '1234', 'French Guiana', true)
+    parse_test('+594694123456', '594', '694', '123456', 'French Guiana', true)
   end
 end
 
