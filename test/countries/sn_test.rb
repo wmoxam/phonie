7bb03ebbec56111744d004b8ae785f799e4d248a
Wesley Moxam <wes@pressly.com>
2013-07-30 13:22:05 -0400

    Fixes Senegal parsing

--- test/countries/sn_test.rb
+++ test/countries/sn_test.rb
@@ -5,10 +5,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 ## Senegal
 class SNTest < Phonie::TestCase
   def test_local
-    parse_test('+221776416455', '221', '7', '76416455', 'Senegal', false)
+    parse_test('+221336416455', '221', '33', '6416455', 'Senegal', false)
   end
 
   def test_mobile
-    parse_test('+221876416455', '221', '8', '76416455', 'Senegal', true)
+    parse_test('+221786416455', '221', '7', '86416455', 'Senegal', true)
   end
 end
