8dcdf78bf38eaee88f8c03ac78f710692f46a2ca
wmoxam <me@wmoxam.com>
2014-01-03 00:13:51 -0500

    Extract number parsing to its own class, tighten up API

--- lib/phonie/country.rb
+++ lib/phonie/country.rb
@@ -1,23 +1,23 @@
+require 'forwardable'
 require 'yaml'
 
 module Phonie
   class Country
-    attr_reader :name, :country_code, :char_2_code, :iso_3166_code, :area_code,
-	    :local_number_format, :mobile_format, :full_number_length,
-	    :number_format 
+    extend Forwardable
+
+    attr_reader :name, :country_code, :char_2_code, :iso_3166_code, :parser
     
     def initialize(params)
       @name          = params[:name]
       @country_code  = params[:country_code]
       @char_2_code   = params[:char_2_code]
       @iso_3166_code = params[:iso_3166_code]
-      @area_code     = params[:area_code]
-      @local_number_format = params[:local_number_format]
-      @mobile_format       = params[:mobile_format]
-      @full_number_length  = params[:full_number_length]
-      @number_format       = params[:number_format]
+      @parser        = Phonie::Parser.new(params)
       @national_dialing_prefix = params[:national_dialing_prefix]
     end
+
+    def_delegators :parser, :is_mobile?, :possible_valid_number?,
+	                    :is_valid_number?, :parse
 
     def self.all
       @@all ||= begin
@@ -51,48 +51,19 @@ module Phonie
       all_by_name[name.downcase] if name
     end
 
-    # detect country from the string entered
-    def self.detect(string, default_country_code, default_area_code)
+    # detect country from the passed phone number
+    def self.detect(phone_number, default_country_code, default_area_code)
       # use the default_country_code to try for a quick match
-      country = find_all_by_phone_code(default_country_code).find do |country|
-        country.matches_full_number?(string) ||
-          country.matches_local_number_with_area_code?(string) ||
-          country.matches_local_number?(string, default_area_code)
+      country = find_all_by_phone_code(default_country_code).find do |c|
+        c.possible_valid_number?(phone_number, default_area_code)
       end
 
       # then search all for a full match
-      country || all.find {|country| country.matches_full_number?(string) }
+      country || all.find {|country| country.is_valid_number?(phone_number) }
     end
 
     def to_s
       name
-    end
-
-    def is_mobile?(number)
-      return true if mobile_format.nil?
-      number =~ mobile_number_regex ? true : false
-    end
-
-    # true if string contains country_code + area_code + local_number
-    def matches_full_number?(string)
-      string =~ full_number_regex && string =~ number_format_regex
-    end
-
-    # true if string contains area_code + local_number
-    def matches_local_number_with_area_code?(string)
-      string =~ area_code_number_regex && string =~ number_format_regex
-    end
-
-    # true if string contains only the local_number, but the default_area_code is valid
-    def matches_local_number?(string, default_area_code)
-      string =~ number_regex && default_area_code =~ area_code_regex
-    end
-
-    def parse(number, default_area_code)
-      parse_full_match(number) ||
-      parse_area_code_match(number) ||
-      parse_with_default(number, default_area_code) ||
-      {}
     end
 
     def national_dialing_prefix
@@ -102,57 +73,7 @@ module Phonie
     end
 
     def valid?
-      !!(name && area_code && local_number_format && number_format)
-    end
-
-    private
-
-    def parse_full_match(number)
-      match = number.match(full_number_regex)
-      return nil unless match
-
-      { area_code: match[2],
-	number:    match[-1] }
-    end
-
-    def parse_area_code_match(number)
-      match = number.match(area_code_number_regex)
-      return nil unless match
-
-      { area_code: match[1],
-	number:    match[-1] }
-    end
-
-    def parse_with_default(number, default_area_code)
-      match = number.match(number_regex)
-      return nil unless match
-
-      { area_code: default_area_code,
-	number:    match[1] }
-    end
-
-    def number_format_regex
-      @number_format_regex ||= Regexp.new("^[+0]?(#{country_code})?(#{number_format})$")
-    end
-
-    def full_number_regex
-      @full_number_regex ||= Regexp.new("^[+]?(#{country_code})(#{area_code})(#{local_number_format})$")
-    end
-
-    def area_code_number_regex
-      @area_code_number_regex ||= Regexp.new("^0?(#{area_code})(#{local_number_format})$")
-    end
-
-    def area_code_regex
-      @area_code_regex ||= Regexp.new("^0?(#{area_code})$")
-    end
-
-    def mobile_number_regex
-      @mobile_number_regex ||= Regexp.new("^(#{mobile_format})$")
-    end
-
-    def number_regex
-      @number_regex ||= Regexp.new("^(#{local_number_format})$")
+      !!(name && parser.valid?)
     end
   end
 end
---
+++ lib/phonie/parser.rb
@@ -0,0 +1,104 @@
+module Phonie
+  class Parser
+    attr_reader :area_code, :country_code, :local_number_format, :mobile_format, :number_format
+
+    def initialize(params)
+      @area_code           = params[:area_code]
+      @country_code        = params[:country_code]
+      @local_number_format = params[:local_number_format]
+      @mobile_format       = params[:mobile_format]
+      @number_format       = params[:number_format]
+    end
+
+    def is_mobile?(number)
+      return true if mobile_format.nil?
+      number =~ mobile_number_regex ? true : false
+    end
+
+    def is_valid_number?(phone_number)
+      matches_full_number?(phone_number)
+    end
+
+    def parse(number, default_area_code)
+      parse_full_match(number) ||
+      parse_area_code_match(number) ||
+      parse_with_default(number, default_area_code) ||
+      {}
+    end
+
+    def possible_valid_number?(phone_number, default_area_code)
+      matches_full_number?(phone_number) ||
+      matches_local_number_with_area_code?(phone_number) ||
+      matches_local_number?(phone_number, default_area_code)
+    end
+
+    def valid?
+      !!(area_code && local_number_format && number_format)
+    end
+
+    private
+
+    # true if string contains country_code + area_code + local_number
+    def matches_full_number?(string)
+      string =~ full_number_regex && string =~ number_format_regex
+    end
+
+    # true if string contains area_code + local_number
+    def matches_local_number_with_area_code?(string)
+      string =~ area_code_number_regex && string =~ number_format_regex
+    end
+
+    # true if string contains only the local_number, but the default_area_code is valid
+    def matches_local_number?(string, default_area_code)
+      string =~ number_regex && default_area_code =~ area_code_regex
+    end
+
+    def parse_full_match(number)
+      match = number.match(full_number_regex)
+      return nil unless match
+
+      { area_code: match[2],
+        number:    match[-1] }
+    end
+
+    def parse_area_code_match(number)
+      match = number.match(area_code_number_regex)
+      return nil unless match
+
+      { area_code: match[1],
+        number:    match[-1] }
+    end
+
+    def parse_with_default(number, default_area_code)
+      match = number.match(number_regex)
+      return nil unless match
+
+      { area_code: default_area_code,
+        number:    match[1] }
+    end
+
+    def number_format_regex
+      @number_format_regex ||= Regexp.new("^[+0]?(#{country_code})?(#{number_format})$")
+    end
+
+    def full_number_regex
+      @full_number_regex ||= Regexp.new("^[+]?(#{country_code})(#{area_code})(#{local_number_format})$")
+    end
+
+    def area_code_number_regex
+      @area_code_number_regex ||= Regexp.new("^0?(#{area_code})(#{local_number_format})$")
+    end
+
+    def area_code_regex
+      @area_code_regex ||= Regexp.new("^0?(#{area_code})$")
+    end
+
+    def mobile_number_regex
+      @mobile_number_regex ||= Regexp.new("^(#{mobile_format})$")
+    end
+
+    def number_regex
+      @number_regex ||= Regexp.new("^(#{local_number_format})$")
+    end
+  end
+end
