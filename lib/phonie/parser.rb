6556d0112214363672682c5e673ffbae14cf5c7e
wmoxam <me@wmoxam.com>
2014-01-06 23:19:37 -0500

    Add some documentation for Parser

--- lib/phonie/parser.rb
+++ lib/phonie/parser.rb
@@ -1,7 +1,15 @@
 module Phonie
+  # Parser is responsible for parsing and verifying phone numbers
   class Parser
     attr_reader :area_code, :country_code, :local_number_format, :mobile_format, :number_format
 
+    # Creates a new phone number parser based on a hash of +params+
+    # The +params+ hash requires
+    # * area_code
+    # * country_code - a number representing the International Subscriber Dialling code
+    # * local_number_format - a regex describing a local phone number (minus country and area code)
+    # * number_format - a regex describing a valid phone number
+    # * mobile_format (optional) - a regex describing a mobile phone number
     def initialize(params)
       @area_code           = params[:area_code]
       @country_code        = params[:country_code]
@@ -10,28 +18,41 @@ module Phonie
       @number_format       = params[:number_format]
     end
 
+    # Test if a phone +number+ might be for a mobile phone
+    # Can produce false positives, and some countries have
+    # portable numbers between landlines and mobile phones
+    # in which case this will always be true
     def is_mobile?(number)
       return true if mobile_format.nil?
       number =~ mobile_number_regex ? true : false
     end
 
+    # Test if +phone_number+ is valid
     def is_valid_number?(phone_number)
       matches_full_number?(phone_number)
     end
 
-    def parse(number, default_area_code)
+    # Parse a pass phone +number+ returning it's area_code & number as a
+    # hash if valid.
+    # Optionally a +default_area_code+ may be passed in order to
+    # allow parsing local number numbers for a known area code
+    def parse(number, default_area_code = nil)
       parse_full_match(number) ||
       parse_area_code_match(number) ||
       parse_with_default(number, default_area_code) ||
       {}
     end
 
-    def possible_valid_number?(phone_number, default_area_code)
+    # Test if a phone number could possibly be valid by testing
+    # if it's matches a number without a country code, and optionally
+    # testing for a local number against a default_area_code
+    def possible_valid_number?(phone_number, default_area_code = nil)
       matches_full_number?(phone_number) ||
       matches_local_number_with_area_code?(phone_number) ||
       matches_local_number?(phone_number, default_area_code)
     end
 
+    # Test that a parser is valid and is capable of parsing phone numbers
     def valid?
       !!(country_code && area_code && local_number_format && number_format)
     end
