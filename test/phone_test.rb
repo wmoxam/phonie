wmoxam/phonie

commit 7c88730e6e15617800db23e34291a2a5ee068781

Removes reliance on cattr. Configuration syntax changed

test/phone_test.rb
@@ -7,16 +7,13 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_number_without_country_code_initialize
-    Phonie::Phone.default_country_code = nil
-
     pn = Phonie::Phone.new '5125486', '91'
     assert !pn.valid?
     assert_equal ["can't be blank"], pn.errors[:country_code]
   end
 
   def test_number_without_area_code_initialize
-    Phonie::Phone.default_country_code = '1'
-    Phonie::Phone.default_area_code = nil
+    Phonie::Phone.configuration.default_country_code = '1'
 
     pn = Phonie::Phone.new '451588'
     assert !pn.valid?
@@ -24,8 +21,8 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_number_with_default_area_code_initialize
-    Phonie::Phone.default_country_code = '385'
-    Phonie::Phone.default_area_code = '47'
+    Phonie::Phone.configuration.default_country_code = '385'
+    Phonie::Phone.configuration.default_area_code = '47'
 
     pn = Phonie::Phone.new '451588'
     assert pn.number == '451588'
@@ -34,7 +31,7 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_number_with_default_country_code_initialize
-    Phonie::Phone.default_country_code = '386'
+    Phonie::Phone.configuration.default_country_code = '386'
 
     pn = Phonie::Phone.new '5125486', '91'
     assert pn.number == '5125486'
@@ -43,7 +40,7 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_number_with_country_code_initialize
-    Phonie::Phone.default_country_code = '387'
+    Phonie::Phone.configuration.default_country_code = '387'
 
     pn = Phonie::Phone.new '5125486', '91', '385'
     assert pn.number == '5125486'
@@ -57,8 +54,6 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_parse_short_without_special_characters_without_country
-    Phonie::Phone.default_country_code = nil
-
     assert_nil Phonie::Phone.parse "0915125486"
 
     assert_raise ArgumentError do
@@ -67,8 +62,6 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_parse_short_with_special_characters_without_country
-    Phonie::Phone.default_country_code = nil
-
     assert_nil Phonie::Phone.parse "091/512-5486"
 
     assert_raise ArgumentError do
@@ -77,31 +70,28 @@ class PhoneTest < Phonie::TestCase
   end
 
   def test_to_s
-    Phonie::Phone.default_country_code = nil
     pn = Phonie::Phone.new '5125486', '91', '385'
     assert pn.to_s == '+385915125486'
   end
 
   def test_to_s_without_country_code
-    Phonie::Phone.default_country_code = '385'
+    Phonie::Phone.configuration.default_country_code = '385'
     pn = Phonie::Phone.new '5125486', '91'
     assert pn.format("0%a%n") == '0915125486'
   end
 
   def test_format_special_with_country_code
-    Phonie::Phone.default_country_code = nil
     pn = Phonie::Phone.new '5125486', '91', '385'
     assert pn.format("+ %c (%a) %n") == '+ 385 (91) 5125486'
   end
 
   def test_format_special_without_country_code
-    Phonie::Phone.default_country_code = '385'
+    Phonie::Phone.configuration.default_country_code = '385'
     pn = Phonie::Phone.new '5125486', '91'
     assert_equal '091/512-5486', pn.format("%A/%f-%l")
   end
 
   def test_format_with_symbol_specifier
-    Phonie::Phone.default_country_code = nil
     pn = Phonie::Phone.new '5125486', '91', '385'
     assert_equal '+385 (0) 91 512 5486', pn.format(:europe)
   end

Readme.rdoc
@@ -23,15 +23,17 @@ If the country or area code isn't given in the string, you must set it, otherwis
     Phonie::Phone.parse '(091) 512 5486', :country_code => '385'
 
 If you feel that it's tedious, set the default country code once (in your config/environment.rb):
-    Phonie::Phone.default_country_code = '385'
+    Phonie::Phone.configuration.default_country_code = '385'
     Phonie::Phone.parse '091/512-5486'
     Phonie::Phone.parse '(091) 512 5486'
 
 Same goes for the area code:
     Phonie::Phone.parse '451-588', :country_code => '385', :area_code => '47'
-or
-    Phonie::Phone.default_country_code = '385'
-    Phonie::Phone.default_area_code = '47'
+or it can be configured in a block
+    Phonie::Phone.configure do |config|
+      config.default_country_code = '385'
+      config.default_area_code = '47'
+    end
 
     Phonie::Phone.parse '451-588'
 

lib/phonie.rb
@@ -1,5 +1,5 @@
 require "phonie/version"
-require "phonie/support" unless defined? ActiveSupport
+require "phonie/configuration"
 require "phonie/phone"
 require "phonie/country"
 require "phonie/railties/validator" if defined? ActiveModel

lib/phonie/configuration.rb
@@ -1,0 +1,13 @@
+require 'singleton'
+
+module Phonie
+  class Configuration
+    include Singleton
+
+    attr_accessor :default_area_code, :default_country_code, :n1_length
+ 
+    def initialize
+      @n1_length = 3
+    end
+  end
+end

lib/phonie/country.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module Phonie
   class Country < Struct.new(:name, :country_code, :char_2_code, :iso_3166_code, :area_code, :local_number_format, :mobile_format, :full_number_length, :number_format, :national_dialing_prefix)
     def self.load

lib/phonie/phone.rb
@@ -1,3 +1,5 @@
+require 'forwardable'
+
 # An object representing a phone number.
 #
 # The phone number is recorded in 3 separate parts:
@@ -9,28 +11,31 @@
 #   Phone.default_country_code
 #   Phone.default_area_code
 #
+
 module Phonie
   class Phone
+    extend Forwardable
+
     EXTENSION = /[ ]*(ext|ex|x|xt|#|:)+[^0-9]*\(*([-0-9]{1,})\)*#?$/i
 
-    attr_accessor :country_code, :area_code, :number, :extension, :country
-    attr_reader :errors
+    attr_reader :country_code, :area_code, :errors, :number, :extension, :country, :configuration
 
-    cattr_accessor :default_country_code
-    cattr_accessor :default_area_code
-    cattr_accessor :named_formats
+    def_delegators :configuration, :default_area_code, :default_country_code, :n1_length,
+                                   :default_area_code=, :default_country_code=, :n1_length=
 
-    # length of first number part (using multi number format)
-    cattr_accessor :n1_length
-    # default length of first number part
-    @@n1_length = 3
 
-    @@named_formats = {
-      :default => "+%c%a%n",
-      :default_with_extension => "+%c%a%nx%x",
-      :europe => '+%c (0) %a %f %l',
-      :us => "(%a) %f-%l"
-    }
+    def self.named_formats  
+      {
+        default: "+%c%a%n",
+        default_with_extension: "+%c%a%nx%x",
+        europe: '+%c (0) %a %f %l',
+        us: "(%a) %f-%l"
+      }
+    end
+
+    def self.configuration
+      Configuration.instance
+    end
 
     def initialize(*hash_or_args)
       if hash_or_args.first.is_a?(Hash)
@@ -40,12 +45,18 @@ module Phonie
         keys = {:number => 0, :area_code => 1, :country_code => 2, :extension => 3, :country => 4}
       end
 
+      @configuration = self.class.configuration
+
       @number       = hash_or_args[ keys[:number] ]
-      @area_code    = hash_or_args[ keys[:area_code] ] || self.default_area_code
-      @country_code = hash_or_args[ keys[:country_code] ] || self.default_country_code
+      @area_code    = hash_or_args[ keys[:area_code] ] || @configuration.default_area_code
+      @country_code = hash_or_args[ keys[:country_code] ] || @configuration.default_country_code
       @extension    = hash_or_args[ keys[:extension] ]
       @country      = hash_or_args[ keys[:country] ]
       @errors = {}
+    end
+
+    def self.configure(&block)
+      yield configuration
     end
 
     def self.parse!(string, options = {})
@@ -59,8 +70,8 @@ module Phonie
     def self.parse(string, options = {})
       return if string.nil?
 
-      options[:country_code] ||= self.default_country_code
-      options[:area_code]    ||= self.default_area_code
+      options[:country_code] ||= configuration.default_country_code
+      options[:area_code]    ||= configuration.default_area_code
 
       extension = extract_extension(string)
       normalized = normalize(string)
@@ -96,12 +107,12 @@ module Phonie
 
     # first n characters of :number
     def number1
-      number[0...self.class.n1_length]
+      number[0...configuration.n1_length]
     end
 
     # everything left from number after the first n characters (see number1)
     def number2
-      n2_length = number.size - self.class.n1_length
+      n2_length = number.size - configuration.n1_length
       number[-n2_length, n2_length]
     end
 
@@ -121,8 +132,8 @@ module Phonie
     #   pn.format(:europe)
     def format(fmt)
       if fmt.is_a?(Symbol)
-        raise ArgumentError.new("The format #{fmt} doesn't exist") unless named_formats.has_key?(fmt)
-        format_number named_formats[fmt]
+        raise ArgumentError.new("The format #{fmt} doesn't exist") unless format_exists?(fmt)
+        format_number self.class.named_formats[fmt]
       else
         format_number(fmt)
       end
@@ -135,12 +146,12 @@ module Phonie
 
     # does this number belong to the default country code?
     def has_default_country_code?
-      country_code == self.class.default_country_code
+      country_code == configuration.default_country_code
     end
 
     # does this number belong to the default area code?
     def has_default_area_code?
-      area_code == self.class.default_area_code
+      area_code == configuration.default_area_code
     end
 
     def valid?
@@ -155,6 +166,10 @@ module Phonie
     end
 
     private
+
+    def format_exists?(format)
+      self.class.named_formats.has_key?(format)
+    end
 
     def validate
       [:country_code, :area_code, :number].each do |field|

lib/phonie/support.rb
@@ -1,39 +1,0 @@
-require 'yaml'
-# support methods to remove dependencies on ActiveSupport
-
-module Accessorize
-  module ClassMethods
-    def cattr_accessor(*syms)
-      syms.flatten.each do |sym|
-        class_eval(<<-EOS, __FILE__, __LINE__)
-          unless defined? @@#{sym}
-            @@#{sym} = nil
-          end
-
-          def self.#{sym}
-            @@#{sym}
-          end
-
-          def #{sym}=(value)
-            @@#{sym} = value
-          end
-
-          def self.#{sym}=(value)
-            @@#{sym} = value
-          end
-
-          def #{sym}
-            @@#{sym}
-          end
-        EOS
-      end
-    end    
-  end
-
-
-  def self.included(receiver)
-    receiver.extend         ClassMethods
-  end
-end
-
-Object.send(:include, Accessorize)

test/countries/bd_test.rb
@@ -9,7 +9,7 @@ class BDTest < Phonie::TestCase
   end
 
   def test_with_default_country
-    Phonie::Phone.default_country_code = '880'
+    Phonie::Phone.configuration.default_country_code = '880'
     parse_test('1715379982', '880', '171', '5379982', 'Bangladesh', true)
     parse_test('1191573647', '880', '119', '1573647', 'Bangladesh', true)
     parse_test('21915736', '880', '2', '1915736', 'Bangladesh', false)

commit 331091dd9008e16f60e198a57e0195a099fa109c

Reimplement ability to customize named formatters
This brings the implementation back in line with what the Readme
indicates is possible.

This regression was introduced in 7c88730.

lib/phonie/formatter.rb
@@ -16,6 +16,13 @@ module Phonie
   class Formatter
     attr_reader :format, :phone_number
 
+    @named_formats = {
+      default: "+%c%a%n",
+      default_with_extension: "+%c%a%nx%x",
+      europe: '+%c (0) %a %f %l',
+      us: "(%a) %f-%l"
+    }
+
     def initialize(params)
       @phone_number = params[:phone_number]
       
@@ -31,12 +38,11 @@ module Phonie
     end
 
     def self.named_formats
-      {
-        default: "+%c%a%n",
-        default_with_extension: "+%c%a%nx%x",
-        europe: '+%c (0) %a %f %l',
-        us: "(%a) %f-%l"
-      }
+      @named_formats
+    end
+
+    def self.named_formats=(named_formats)
+      @named_formats = named_formats
     end
 
     def to_s

test/phone_test.rb
@@ -96,6 +96,24 @@ class PhoneTest < Phonie::TestCase
     assert_equal '+385 (0) 91 512 5486', pn.format(:europe)
   end
 
+  def test_format_with_custom_symbol_specifier
+    Phonie::Formatter.named_formats[:internal] = 'ext.%x'
+    pn = Phonie::Phone.new '5125486', '91', '385', '1234'
+    assert_equal 'ext.1234', pn.format(:internal)
+    Phonie::Formatter.named_formats.delete(:internal)
+  end
+
+  def test_format_with_complete_named_formats_replacement
+    old_formatters = Phonie::Formatter.named_formats
+    Phonie::Formatter.named_formats = { country:'%c' }
+    pn = Phonie::Phone.new '5125486', '91', '385', '1234'
+    assert_equal '385', pn.format(:country)
+    assert_raise ArgumentError do
+      pn.format(:europe)
+    end
+    Phonie::Formatter.named_formats = old_formatters
+  end
+
   def test_valid
     assert Phonie::Phone.valid?('915125486', :country_code => '385')
     assert Phonie::Phone.valid?('385915125486')