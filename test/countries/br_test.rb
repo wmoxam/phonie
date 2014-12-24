aa7e72a53dcc3420527313bc6c371bce79bdef63
Marcos Piccinini <x@nofxx.com>
2014-12-24 19:40:17 -0200

    fix brazil

--- test/countries/br_test.rb
+++ test/countries/br_test.rb
@@ -4,35 +4,39 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 class BRTest < Phonie::TestCase
 
   def test_local
-    parse_test('+55 91 12345678', '55', "91", '12345678', 'Brazil', false)
+    parse_test('+55 91 12345678', '55', '91', '12345678', 'Brazil', false)
   end
 
   def test_sao_paulo
-    parse_test('+55 11 12345678', '55', "11", '12345678', 'Brazil', false)
+    parse_test('+55 11 12345678', '55', '11', '12345678', 'Brazil', false)
   end
 
   def test_minas_gerais
-    parse_test('+55 35 12345678', '55', "35", '12345678', 'Brazil', false)
+    parse_test('+55 35 12345678', '55', '35', '12345678', 'Brazil', false)
   end
 
   def test_area_with_zero
-    parse_test('+55 35 12345678', '55', "35", '12345678', 'Brazil', false)
+    parse_test('+55 35 12345678', '55', '35', '12345678', 'Brazil', false)
+  end
+
+  def test_area_max
+    parse_test('+55 99 59995678', '55', '99', '59995678', 'Brazil', false)
   end
 
   def test_paid_toll
-    parse_test('+55 0300 12 345 67', '55', "0300", '1234567', 'Brazil', false)
+    parse_test('+55 0300 12 345 67', '55', '0300', '1234567', 'Brazil', false)
   end
 
   def test_donation_service
-    parse_test('+55 0500 12 345 67', '55', "0500", '1234567', 'Brazil', false)
+    parse_test('+55 0500 12 345 67', '55', '0500', '1234567', 'Brazil', false)
   end
 
   def test_toll_free
-    parse_test('+55 0800 12 345 67', '55', "0800", '1234567', 'Brazil', false)
+    parse_test('+55 0800 12 345 67', '55', '0800', '1234567', 'Brazil', false)
   end
 
   def test_paid_service
-    parse_test('+55 0900 123 4567', '55', "0900", '1234567', 'Brazil', false)
+    parse_test('+55 0900 123 4567', '55', '0900', '1234567', 'Brazil', false)
   end
 
   #
@@ -58,14 +62,26 @@ class BRTest < Phonie::TestCase
   end
 
   def test_mobile_low_range
-    parse_test('55 11 6101 4567', '55', '11', '61014567', 'Brazil', true)
+    parse_test('55 35 6101 4567', '55', '35', '61014567', 'Brazil', true)
   end
 
   def test_mobile_high_range
     parse_test('55 35 9991 4567', '55', '35', '99914567', 'Brazil', true)
   end
 
-  def test_mobile_with_9
-    parse_test('55 12 98765 5678', '55', '12', '987655678', 'Brazil', true)
+  def test_mobile_area_11_with_9
+    parse_test('55 11 99765 5678', '55', '11', '997655678', 'Brazil', true)
+  end
+
+  def test_mobile_area_16_with_9
+    parse_test('55 16 98765 5678', '55', '16', '987655678', 'Brazil', true)
+  end
+
+  def test_mobile_area_19_with_9
+    parse_test('55 19 98765 5678', '55', '19', '987655678', 'Brazil', true)
+  end
+
+  def test_mobile_area_21_with_9
+    parse_test('55 21 98765 5678', '55', '21', '987655678', 'Brazil', true)
   end
 end
