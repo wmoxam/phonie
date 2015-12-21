fec87c6c0ad543dde0ece940d0e6973a61f65b69
Matheus Mina <mfbmina@gmail.com>
2015-12-21 17:41:06 -0200

    create a validator for mobile

--- lib/phonie/railties/validator.rb
+++ lib/phonie/railties/validator.rb
@@ -5,3 +5,9 @@ class PhoneValidator < ActiveModel::EachValidator
     object.errors.add(attribute, :invalid_phone_number) unless Phonie::Phone.valid?(value)
   end
 end
+
+class MobilePhoneValidator < ActiveModel::EachValidator
+  def validate_each(object, attribute, value)
+    object.errors.add(attribute, :invalid_mobile_phone_number) unless Phonie::Phone.valid?(value) && Phonie::Phone.is_mobile?(value)
+  end
+end
