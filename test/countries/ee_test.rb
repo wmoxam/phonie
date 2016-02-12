4f87ab37b468323c260301a12c2d826912071461
Conor Hynes <conor.hynes.personal@gmail.com>
2016-02-12 16:47:52 +0000

    I wanted to fix estonia, so I changed the regex
    so the whole country can register their phone numbers on our website.

--- test/countries/ee_test.rb
+++ test/countries/ee_test.rb
@@ -7,6 +7,10 @@ class EETest < Phonie::TestCase
   end
 
   def test_mobile
+    parse_test('+3725123456', '372', '5', '123456', 'Estonia', true)
     parse_test('+37251234567', '372', '5', '1234567', 'Estonia', true)
+    parse_test('+37281123456', '372', '81', '123456', 'Estonia', true)
+    parse_test('+37282123456', '372', '82', '123456', 'Estonia', true)
+    parse_test('+37283123456', '372', '83', '123456', 'Estonia', true)
   end
 end
