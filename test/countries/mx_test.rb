8f7b9283041b844c626b5cb3b704ad87289ba6f0
Conor Hynes <conor.hynes.personal@gmail.com>
2016-04-18 17:48:52 +0100

    Adding more area codes to mexico

--- test/countries/mx_test.rb
+++ test/countries/mx_test.rb
@@ -5,13 +5,13 @@ class MXTest < Phonie::TestCase
   def test_local
     parse_test('+52 33 36159895',  '52', '33',  '36159895', 'Mexico', false)
     parse_test('+52 686 3615989',  '52', '686',  '3615989', 'Mexico', false)
+    parse_test('+52 482 1234567','52','482','1234567','Mexico',false)
   end
 
   def test_mobile
     # Mobile phones have extra 1 in front
     parse_test('+52 1 55 12345678', '52', '155', '12345678', 'Mexico', true)
     parse_test('+52 1 444 1234567', '52', '1444', '1234567', 'Mexico', true)
-    parse_test('+52 1 624 123 4567', '52', '1624', '1234567', 'Mexico', true)
-
+    parse_test('+52 1 624 1234567', '52', '1624', '1234567', 'Mexico', true)
   end
 end
