303fbb3f97e5f953883b3f2d4463bff61d2607e7
David Ramalho <dramalho@gmail.com>
2016-05-13 15:28:40 +0100

    Remove the CU

--- test/countries/pa_test.rb
+++ test/countries/pa_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 ## Panama
-class CUTest < Phonie::TestCase
+class PATest < Phonie::TestCase
   def test_local
     parse_test('+50741869702', '507', '4', '1869702', 'Panama', false)
   end
