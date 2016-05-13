ea697f5aef6d3686cef12221f9971f031d47c3f5
David Ramalho <dramalho@gmail.com>
2016-05-13 15:10:35 +0100

    Add Panama phone rules. Specs

---
+++ test/countries/pa_test.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+## Panama
+class CUTest < Phonie::TestCase
+  def test_local
+    parse_test('+50741869702', '507', '4', '1869702', 'Panama', false)
+  end
+
+  def test_mobile
+    parse_test('+50762826561', '507', '6', '2826561', 'Panama', true)
+  end
+
+end
